3a75242ec595f9f2301e990bcc0f4486e0c88767
Thanuja Aalavala <66903220+thanuja-reddy@users.noreply.github.com>
2020-07-15 10:25:44 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+//VARIOUS DATATYPES IN JAVASCRIPT
+var length=16;//Number
+var lastName="Aalavala";//string
+var x={firstName:"thanu",lastName:"Aalavala"};//object
+
+var x;//here x is undefined
+x=5;//here x is number
+x="letsupgrade";//here x is string
